0bb103b012b83801b87831eb7626fe1f637ab4f3
Gary Lai <garylai1990@gmail.com>
2021-05-26 15:01:03 +0800

    :wrench: add my experience

---
+++ src/data/experience.ts
@@ -0,0 +1,22 @@
+const Experience = [
+  {
+    company: "Marigen Inc",
+    start: "2019-05-01",
+    end: "2021-03-31",
+    location: "San Francisco, USA",
+    title: "Co-founder, Software Engineer",
+    product: "Machiyami",
+    url: "https://machiyami.com",
+  },
+  {
+    company: "Jungllle Inc",
+    start: "2019-05-01",
+    end: "2021-03-31",
+    location: "Taipei Taiwan",
+    title: "Co-founder, Software Engineer",
+    product: "StickerHD",
+    url: "https://stickerhd.com",
+  },
+];
+
+export default Experience;
